7fc5e7e554d1ab1dd908ac4ca7e9d8be5a63e703
yzbao <yuezhongbao@huohua.cn>
2024-02-26 13:50:33 +0800

    feat: done！

--- createYzbaoApp.js
+++ createYzbaoApp.js
@@ -3,6 +3,8 @@ const program = new Command();
 const packageJson = require('./package.json');
 const inquirer = require('inquirer');
 const download = require('download-git-repo');
+const path = require('path');
+const spawn = require('cross-spawn');
 
 function init() {
   // 1. 定义npx create-yzbao-app 命令
@@ -31,22 +33,40 @@ function createYzbaoApp(name, options) {
     message: '请选择要使用的模版'
   }]).then(answers => {
     console.log('已选择',answers);
-    downloadTemplate(answers.template);
+    downloadTemplate(name,answers.template);
   }).catch(error => {});
   
 }
 
-function downloadTemplate(template) {
+function downloadTemplate(name, template) {
   // 1. 下载模版
   // 2. 将模版解压到当前命令执行的目录
-  console.log('正在下载模版', template);
-  download('direct:https://github.com/yzbaoo/cra-template-recoil.git', process.cwd(), {clone: false}, (err) => {
+  const target = path.resolve(process.cwd(), name);
+  console.log('target', target);
+  download('yzbaoo/cra-template-recoil', target, {clone: false}, (err) => {
     if (err) {
       console.log('下载失败', err);
     } else {
       console.log('下载成功');
+      installDependencies(target);
     }
   });
 }
+
+function installDependencies(target) {
+  // 1. 进入到创建的项目目录
+  // 2. 执行npm install或者yarn install
+  const command = 'yarn';
+  const args = ['install'];
+  const child = spawn(command, args, {stdio: 'inherit', cwd: target});
+  child.on('close', code => {
+    if (code !== 0) {
+      console.error(`安装失败`);
+      return;
+    }
+    console.log(`安装成功`);
+  });
+}
+
 
 module.exports = {init};
